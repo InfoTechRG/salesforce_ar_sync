934c20e6a6ee472831aa01e07bc40cddd5dc698a
Pavel Khlustikov <pavel.khlustikov@gmail.com>
2013-03-14 14:11:58 +0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 SalesforceArSync::Engine.routes.draw do
-  match '/sf_soap/delete' => 'salesforce_ar_sync::soap_message#delete', :via => :post
-  match '/sf_soap/*klass' => 'salesforce_ar_sync::soap_message#sync_object', :via => :post
+  match '/sf_soap/delete' => 'salesforce_ar_sync/soap_message#delete', :via => :post
+  match '/sf_soap/*klass' => 'salesforce_ar_sync/soap_message#sync_object', :via => :post
 end
