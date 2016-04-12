5e138deac284c8881681fd906ae1530c74c8c814
Tayler Phillips <tphillips@infotech.com>
2016-04-12 16:23:56 -0400

    Added the deletion map configuration and test.

--- lib/salesforce_ar_sync/engine.rb
+++ lib/salesforce_ar_sync/engine.rb
@@ -3,7 +3,7 @@ module SalesforceArSync
     initializer "salesforce_ar_sync.load_app_instance_data" do |app|
       SalesforceArSync.setup do |config|
         config.app_root = app.root
-        
+
         #Load the configuration from the environment or a yaml file or disable it if no config present
         SalesforceArSync.config = Hash.new
         #load the config file if we have it
@@ -13,6 +13,7 @@ module SalesforceArSync
           SalesforceArSync.config["SYNC_ENABLED"] = config['sync_enabled']
           SalesforceArSync.config["IP_RANGES"] = config['ip_ranges'].split(',').map{ |ip| ip.strip }
           SalesforceArSync.config["NAMESPACE_PREFIX"] = config['namespace_prefix']
+          SalesforceArSync.config['DELETION_MAP'] = config['deletion_map'].stringify_keys
         end
 
         #if we have ENV flags prefer them
@@ -20,6 +21,7 @@ module SalesforceArSync
         SalesforceArSync.config["SYNC_ENABLED"] = ENV["SALESFORCE_AR_SYNC_SYNC_ENABLED"] if ENV.include? "SALESFORCE_AR_SYNC_SYNC_ENABLED"
         SalesforceArSync.config["IP_RANGES"] = ENV["SALESFORCE_AR_SYNC_IP_RANGES"].split(',').map{ |ip| ip.strip } if ENV["SALESFORCE_AR_SYNC_IP_RANGES"]
         SalesforceArSync.config["NAMESPACE_PREFIX"] = ENV["SALESFORCE_AR_NAMESPACE_PREFIX"] if ENV["SALESFORCE_AR_NAMESPACE_PREFIX"]
+        SalesforceArSync.config['DELETION_MAP'] = ENV['DELETION_MAP'] if ENV['DELETION_MAP']
 
         #do we have valid config options now?
         if !SalesforceArSync.config["ORGANIZATION_ID"].present? || SalesforceArSync.config["ORGANIZATION_ID"].length != 18
--- spec/db/schema.rb
+++ spec/db/schema.rb
@@ -11,6 +11,15 @@ ActiveRecord::Schema.define(:version => 1) do
     t.timestamps
   end
 
+  create_table 'vendors', force: true do |t|
+    t.string :name
+    t.string :salesforce_id
+    t.datetime :salesforce_updated_at
+    t.string :sync_inbound_delete
+    t.string :sync_outbound_delete
+    t.timestamps null: false
+  end
+
   create_table "users", :force => true do |t|
     t.timestamps
   end
