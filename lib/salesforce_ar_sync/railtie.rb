c6bf966c55f5df1938bbc34afbc395c41addad54
Michael Brawn <michaelbrawn@gmail.com>
2018-07-31 09:28:56 -0400

    Use XmlParser

--- lib/salesforce_ar_sync/railtie.rb
+++ lib/salesforce_ar_sync/railtie.rb
@@ -1,7 +1,7 @@
 module SalesforceArSync
   class Railtie < Rails::Railtie
     initializer 'salesforce_ar_sync.insert_middleware' do |app|
-      app.config.middleware.insert_after ActionPack::ParamsParser, ActionPack::XmlParamsParser
+      app.config.middleware.insert_after ActionDispatch::ParamsParser, ActionPack::XmlParser
     end
   end
 end
