2c9fd8434193e16222b658a802c2b28f7484c92f
James Haig <jhaig@infotech.com>
2021-05-13 15:02:53 -0400

    .to_json attributes to allow activesupport to be able to serialize any datatype

--- lib/salesforce_ar_sync/salesforce_object_sync.rb
+++ lib/salesforce_ar_sync/salesforce_object_sync.rb
@@ -5,7 +5,7 @@ module SalesforceArSync
       web_object = "#{web_object_name}".constantize.find_by_salesforce_id salesforce_id
       # object exists in salesforce if we call its system_mod_stamp
       if (system_mod_stamp = web_object.system_mod_stamp)
-        web_object.salesforce_update_object(attributes)
+        web_object.salesforce_update_object(JSON.parse(attributes))
         web_object.update_attribute(:salesforce_updated_at, system_mod_stamp)
       end
     end
